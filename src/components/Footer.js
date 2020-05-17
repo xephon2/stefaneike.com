69b39683aab1c691e152dd13c6a4d7346bf565b8
Stefan Eike <stefan.eike@mailbox.org>
2020-05-17 21:47:12 +0200

    Set social icons and links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,9 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import logo from '../img/logo.svg'
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
+import linkedin from '../img/social/linkedin.svg'
 import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+import xing from '../img/social/xing.svg'
 
 const Footer = class extends React.Component {
   render() {
@@ -74,32 +73,27 @@ const Footer = class extends React.Component {
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
+                <a title="linkedin" href="https://www.linkedin.com/in/stefan-eike">
                   <img
-                    src={facebook}
-                    alt="Facebook"
+                    className="fas fa-lg"
+                    src={linkedin}
+                    alt="Linkedin"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="twitter" href="https://twitter.com">
+                <a title="xing" href="https://www.xing.com/profile/Stefan_Eike/cv">
+                  <img
+                    className="fas fa-lg"
+                    src={xing}
+                    alt="Xing"
+                    style={{ width: '1em', height: '1em' }}
+                  />
+                </a>
+                <a title="twitter" href="https://twitter.com/stefaneike">
                   <img
                     className="fas fa-lg"
                     src={twitter}
                     alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
