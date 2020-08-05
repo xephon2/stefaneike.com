c7fe7ba87e1d98ec9f53390b23ad07114ba64cfd
Stefan Eike <stefan.eike@mailbox.org>
2020-08-05 21:19:09 +0200

    Fix link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -34,7 +34,7 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/products">
+                      <Link className="navbar-item" to="/services">
                         Products
                       </Link>
                     </li>
