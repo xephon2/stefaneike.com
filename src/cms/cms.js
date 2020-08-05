cb201713ccd9d6f5263e33c37767d5d5e954f6fb
Stefan Eike <stefan.eike@mailbox.org>
2020-08-05 21:12:53 +0200

    Fix broken template

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -12,5 +12,5 @@ import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
+CMS.registerPreviewTemplate('services', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
